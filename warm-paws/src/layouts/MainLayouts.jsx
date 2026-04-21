51dc4b2ed1d1c7ab72436517b716a9f2aa74915a
lamifaharia <lamifaharia@gmail.com>
2026-04-22 00:40:32 +0600

    fixed the structure

---
+++ warm-paws/src/layouts/MainLayouts.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Toaster } from "react-hot-toast";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+      <Toaster />
+    </div>
+  );
+};
+
+export default MainLayout;
